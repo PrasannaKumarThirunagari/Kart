014d82ebe59fa0b856637630e5c86f68cc288166
PrasannaKumarThirunagari <prasannakumarthirunagari@gmail.com>
2023-07-29 10:26:02 +0530

    EntityFramework Configuration

---
+++ MyVillageKartAPI/Data/KartContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MyVillageKartAPI.EntityModels;
+
+namespace MyVillageKartAPI.Data;
+
+public partial class KartContext : DbContext
+{
+    public KartContext()
+    {
+    }
+
+    public KartContext(DbContextOptions<KartContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<UserDetail> UserDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("SERVER=SAHASRATHIRUNAG;DATABASE=Kart;Integrated Security=True;Encrypt=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserDetail>(entity =>
+        {
+            entity.HasKey(e => e.UserId);
+
+            entity.Property(e => e.UserId)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("UserID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Mobile)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.Mobile2)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MyVillageKartAPI/EntityModels/UserDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyVillageKartAPI.EntityModels;
+
+public partial class UserDetail
+{
+    public string UserId { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public string? MiddleName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Mobile { get; set; }
+
+    public string? Mobile2 { get; set; }
+
+    public string Password { get; set; } = null!;
+}
